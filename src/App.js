4d72a80c1904dac4ba285c643c69217b9ee56866
biruk-coder <birukassefa512@gmail.com>
2023-05-19 01:21:25 +0300

    apple

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react"
+import Header from "./Header";
+import Footer from "./Footer";
+import Body1 from "./Body1";
+import Body2 from "./Body2";
+import Body3 from "./Body3";
+import Body4 from "./Body4";
+import Body5 from "./Body5";
+import Body6 from "./Body6";
+import Body7 from "./Body7";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <br></br>
+      <br></br>
+      <br></br>
+      <Header/>
+      <Body1/>
+      <Body2/>
+      <Body3/>
+      <Body4/>
+      <Body5/>
+      <Body6/>
+      <Body7/>
+      <Footer/>
     </div>
   );
 }
-
 export default App;
